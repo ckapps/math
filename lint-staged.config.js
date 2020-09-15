c04928c510b6ad79437ea74d4f55b807fd1d92f6
christophka <chris@ckapp.net>
2020-09-15 13:33:38 +0200

    chore: removed markdown files from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   'src/**/*.ts': ['prettier --write'],
   '*.js': 'eslint --cache --fix',
   '*.ts': ['eslint --fix'],
-  '*.{js,css,md}': 'prettier --write',
+  '*.{js,css}': 'prettier --write',
 };
