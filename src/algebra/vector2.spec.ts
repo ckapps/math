bb046436c7beb7ddd0053613ec0449bc384872b1
christophka <chris@ckapp.net>
2020-09-16 13:06:36 +0200

    chore: add some unit tests for vectors

--- src/algebra/vector2.spec.ts
+++ src/algebra/vector2.spec.ts
@@ -1,11 +1,25 @@
 import { Vector2 } from './vector2';
+import { create } from './vector_base';
 
 describe('algebra/vector2', () => {
-  it('should create', () => {
-    const x = 0;
-    const y = 0;
-    const v = new Vector2(x, y);
-    expect(v.components[0]).toBe(x);
-    expect(v.components[1]).toBe(y);
+  describe('create', () => {
+    it('should create', () => {
+      const v = new Vector2(0, 0);
+      expect(v).toBeDefined();
+    });
+
+    it('should register factory for self', () => {
+      const v = create([0, 0]);
+      expect(v instanceof Vector2).toBeTruthy();
+    });
+  });
+
+  describe('components', () => {
+    it('should create', () => {
+      const x = 0;
+      const y = 0;
+      const v = new Vector2(x, y);
+      expect(v.components).toEqual([x, y]);
+    });
   });
 });
--- src/algebra/vector3.spec.ts
+++ src/algebra/vector3.spec.ts
@@ -1,10 +1,34 @@
+import { create } from './vector_base';
 import { Vector } from './vector';
+
 import { Vector3 } from './vector3';
 
 describe('algebra/vector3', () => {
   function compare(a: Vector3, b: Vector3) {
     expect(Vector.equals(a, b)).toBe(true);
   }
+
+  describe('create', () => {
+    it('should create', () => {
+      const v = new Vector3(0, 0, 0);
+      expect(v).toBeDefined();
+    });
+
+    it('should register factory for self', () => {
+      const v = create([0, 0, 0]);
+      expect(v instanceof Vector3).toBeTruthy();
+    });
+  });
+
+  describe('components', () => {
+    it('should create', () => {
+      const x = 1;
+      const y = 2;
+      const z = 3;
+      const v = new Vector3(x, y, z);
+      expect(v.components).toEqual([x, y, z]);
+    });
+  });
 
   describe('cross', () => {
     // For basis vectors
