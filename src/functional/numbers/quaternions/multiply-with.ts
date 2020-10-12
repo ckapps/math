59a8599a74c4380a03d1fb1f72bcc677dc39a1a8
christophka <chris@ckapp.net>
2020-10-12 20:35:10 +0200

    docs: multiply-with for quaternions

--- src/functional/numbers/quaternions/multiply-with.ts
+++ src/functional/numbers/quaternions/multiply-with.ts
@@ -1,6 +1,15 @@
 import { OperatorFunction } from '../../types/operator-function';
 import { Quaternion, multiply } from '../../../numbers/quaternion';
 
+/**
+ * @param other The other quaternion
+ *
+ * @returns
+ * A function that takes a quaternion as an argument.
+ * When called with an quaternion `q`, the function
+ * will multiply `q` with `other`, returning a new
+ * quaternion.
+ */
 export function multiplyWith(
   other: Readonly<Quaternion>,
 ): OperatorFunction<Readonly<Quaternion>, Quaternion> {
