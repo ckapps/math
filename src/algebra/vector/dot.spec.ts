a8ff382be55e69fda82697c38a3adb813525ea63
christophka <chris@ckapp.net>
2021-07-01 01:45:02 +0200

    feat(vector): add dotBy

--- src/algebra/vector/dot.spec.ts
+++ src/algebra/vector/dot.spec.ts
@@ -1,8 +1,16 @@
-import { dot } from './dot';
+import { dot, dotBy } from './dot';
 
 describe('algebra/vector/dot', () => {
   it('should return dot', () => {
     const r = dot([0, 0, 0], [1, 1, 1]);
     expect(r).toBe(0);
+  });
+
+  describe('dotBy', () => {
+    const f = dotBy([1, 1, 1]);
+    it('should return dot product', () => {
+      const r = f([0, 0, 0]);
+      expect(r).toBe(0);
+    });
   });
 });
--- src/algebra/vector/dot.ts
+++ src/algebra/vector/dot.ts
@@ -16,3 +16,17 @@ import { vectorNr } from './vector.types';
 export function dot(a: vectorNr, b: vectorNr): number {
   return sum(map(multiply)([a, b]));
 }
+
+/**
+ * @param other Other vector by which the dot product should by calculated
+ *
+ * @returns
+ * A function that takes a vector and that returns the dot product
+ * of this vector and `other`.
+ *
+ * @example
+ * dotBy(b)(a) === dot(a, b)
+ */
+export function dotBy<T extends vectorNr>(other: T) {
+  return (a: T) => dot(a, other);
+}
