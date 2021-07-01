4d8fc1c368c49744dacab71c251254a4bfef87b1
christophka <chris@ckapp.net>
2021-07-01 02:07:01 +0200

    fix(vector): typing for divide

--- src/algebra/vector/divide.ts
+++ src/algebra/vector/divide.ts
@@ -1,5 +1,5 @@
 import { scale } from './scale';
-import { vectorN, vectorNr } from './vector.types';
+import { vectorNr } from './vector.types';
 
 /**
  * Divides all components of the vector by the given `scalar`.
@@ -9,6 +9,9 @@ import { vectorN, vectorNr } from './vector.types';
  * @returns
  * A new vector with each component inverse scaled by `scalar`.
  */
-export function divide(v: vectorNr, scalar: number): vectorN {
+export function divide<T extends vectorNr>(v: T, scalar: number): T;
+export function divide(v: vectorNr, scalar: number): vectorNr;
+
+export function divide(v: vectorNr, scalar: number): vectorNr {
   return scale(v, 1 / scalar);
 }
