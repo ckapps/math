1b42076c42a1ca9c6875210e3c2496e42693c3fb
christophka <chris@ckapp.net>
2021-11-09 00:29:15 +0100

    perf: remove abs from calculation

--- src/fn/base/mod-wrap.ts
+++ src/fn/base/mod-wrap.ts
@@ -8,15 +8,15 @@ import { mod } from './mod';
  * so that negative values will be wrapped to positive modulo values.
  *
  * @example
- * cycleIndex(3)(-3) === 0
- * cycleIndex(3)(-2) === 1
- * cycleIndex(3)(-1) === 2
- * cycleIndex(3)( 0) === 0
- * cycleIndex(3)( 1) === 1
- * cycleIndex(3)( 2) === 2
- * cycleIndex(3)( 3) === 3
+ * modWrap(3)(-3) === 0
+ * modWrap(3)(-2) === 1
+ * modWrap(3)(-1) === 2
+ * modWrap(3)( 0) === 0
+ * modWrap(3)( 1) === 1
+ * modWrap(3)( 2) === 2
+ * modWrap(3)( 3) === 3
  */
 export function modWrap(modulus: number) {
   const modOperator = mod(modulus);
-  return (n: number) => modOperator(Math.abs(modulus + modOperator(n)));
+  return (n: number) => modOperator(modulus + modOperator(n));
 }
