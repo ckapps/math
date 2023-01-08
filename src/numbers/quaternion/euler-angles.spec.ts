ccbbd35557812d4dc874ddf04d407e52df7739bf
christophka <chris@ckapp.net>
2023-01-08 23:05:01 +0100

    test(numbers): add inhomogenous test

--- src/numbers/quaternion/euler-angles.spec.ts
+++ src/numbers/quaternion/euler-angles.spec.ts
@@ -2,7 +2,8 @@ import { describe, expect, it } from '@jest/globals';
 import { fromEulerZXY, toEulerAngles } from './euler-angles';
 
 describe('algebra/quaternion', () => {
-  const mockQuaternion = { w: 0, x: 1, y: 0, z: 0 };
+  const quaternionHom = { w: 0, x: 1, y: 0, z: 0 };
+  const quaternionInhom = { w: 0, x: 5, y: 0, z: 0 };
 
   it('fromEulerZXY', () => {
     const r = fromEulerZXY([0, 0, 0]);
@@ -14,11 +15,21 @@ describe('algebra/quaternion', () => {
     expect(r.z).toBe(0);
   });
 
-  it('toEulerAngles', () => {
-    const [x, y, z] = toEulerAngles(mockQuaternion);
+  describe('toEulerAngles', () => {
+    it('should transform to euler angles without homogenous', () => {
+      const [x, y, z] = toEulerAngles(quaternionHom);
 
-    expect(x).toBe(0);
-    expect(y).toBeCloseTo(0);
-    expect(z).toBe(Math.PI);
+      expect(x).toBe(0);
+      expect(y).toBeCloseTo(0);
+      expect(z).toBe(Math.PI);
+    });
+
+    it('should transform to euler angles with homogenous', () => {
+      const [x, y, z] = toEulerAngles(quaternionInhom, false);
+
+      expect(x).toBe(Math.PI);
+      expect(y).toBeCloseTo(0);
+      expect(z).toBe(0);
+    });
   });
 });
